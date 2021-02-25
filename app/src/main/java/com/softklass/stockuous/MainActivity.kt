be4c6876a0eecfee2faaca23c48373f285f14060
Joshy <josh.ludahl@gmail.com>
2021-02-24 23:11:56 -0800

    init

--- app/src/main/java/com/softklass/stockuous/MainActivity.kt
+++ app/src/main/java/com/softklass/stockuous/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.softklass.stockuous
 
+import android.graphics.Color.parseColor
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.softklass.stockuous.ui.theme.StockuousTheme
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +20,10 @@ class MainActivity : AppCompatActivity() {
         setContent {
             StockuousTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = Color.Magenta) {
-                    Lists()
+                Surface(color = Color(blackish.value)) {
+                    Box(Modifier.fillMaxSize()) {
+                        Lists()
+                    }
                 }
             }
         }
@@ -28,16 +32,26 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", color = Color(tealish.value))
 }
 
+@Preview
 @Composable
 fun Lists() {
-    Column () {
-        Greeting(name = "Android")
-        Greeting(name = "World")
-        Greeting(name = "Oregon")
+    Row {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Greeting(name = "Android - C")
+            Greeting(name = "World")
+            Greeting(name = "Oregon")
+        }
+
+        Column(modifier = Modifier.padding(16.dp)) {
+            Greeting(name = "Android - C")
+            Greeting(name = "World")
+            Greeting(name = "Oregon")
+        }
     }
+
 }
 
 @Preview(showBackground = true)
@@ -47,3 +61,10 @@ fun DefaultPreview() {
         Greeting("Android")
     }
 }
+
+// Colors
+val black = Color(parseColor("#000000"))
+val blackish = Color(parseColor("#222831"))
+val grayish = Color(parseColor("#393e46"))
+val tealish = Color(parseColor("#00adb5"))
+val light_gray = Color(parseColor("#eeeeee"))
